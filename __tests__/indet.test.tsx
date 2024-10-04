0bcd9f8f6d5347aa6ac4cb50fb69bc38698b8c1d
Jasmine Bai <baijm91@gmail.com>
2024-10-05 10:26:19 +1300

    test: add unit tests for home page

---
+++ __tests__/indet.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import Home from '@/pages';
+
+jest.mock('@/HorseList', () => {
+  function MockHorseList() {
+    return <div data-testid="horse-list">Mock Horse List</div>;
+  }
+  return MockHorseList;
+});
+
+describe('Home Page', () => {
+  it('should render the HorseList component', () => {
+    render(<Home />);
+
+    const horseList = screen.getByTestId('horse-list');
+    expect(horseList).toBeInTheDocument();
+  });
+});
